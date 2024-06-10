f6bd7ab85c35ac99e75628cf8b2f5d8c4cd72159
orlowski11 <pawel.orlowski@studenci.collegiumwitelona.pl>
2024-06-10 14:30:41 +0200

    added login via github with socialite

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class UserController extends Controller
+{
+    public function login(){
+        $githubUser = Socialite::driver('github')->user();
+    
+        $user = User::updateOrCreate([
+            'github_id' => $githubUser->id,
+        ], [
+            'name' => $githubUser->nickname,
+            'email' => $githubUser->email,
+            'github_token' => $githubUser->token,
+            'github_refresh_token' => $githubUser->refreshToken,
+        ]);
+    
+        Auth::login($user);
+    
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,12 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Response;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\UserController;
 
 Route::get("/", fn(): Response => inertia("Welcome"));
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+Route::get('/auth/callback', [UserController::class, 'login']);
