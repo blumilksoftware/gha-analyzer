ac77275c34a90fedf0bf7bad0f68c01decda1a3a
Wiktor-Wojciechowski <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-06-14 14:28:42 +0200

    added home placeholder+route

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { Link, Head } from '@inertiajs/vue3'
+</script>
+<template>
+    <Head>Home</Head>
+    <h1>Home</h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\GithubController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Response;
 
-Route::get("/", fn(): Response => inertia("Welcome"));
+//Route::get("/", fn(): Response => inertia("Welcome"));
+Route::inertia('/', 'Home');
 
 Route::get("/auth/redirect", [GithubController::class, "redirect"]);
 Route::get("/auth/callback", [GithubController::class, "callback"]);
