12f62db691da7ff2c215e2191807967fc68d812a
Wiktor-Wojciechowski <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-06-19 09:03:51 +0200

    changed table, testing data passing

---
+++ resources/js/Pages/Hello.vue
@@ -0,0 +1,12 @@
+<script>
+export const hello = 'Hello from Hello.vue!';
+
+export default {
+  data() {
+    return {
+      // This could be used locally within the component
+      localHello: hello
+    };
+  }
+};
+</script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,14 @@
-<script setup>
+<script >
 import { Head } from '@inertiajs/vue3'
+import { hello } from './Hello.vue';
+
+export default {
+  data() {
+    return {
+      hello
+    };
+  }
+};
 
 </script>
 <style>
@@ -12,6 +21,7 @@ import { Head } from '@inertiajs/vue3'
     <Head>
         <title>GitHub Action simple analyzer</title>
     </Head>
+    <h1>{{hello}}</h1>
     <div class="container my-4 mx-auto bg-white rounded-xl p-4 shadow flex">
         <div class="flex-1 p-4">
             <textarea class="w-full text-xs font-mono shadow border rounded-xl p-3" :value="logs" rows="20"></textarea>
