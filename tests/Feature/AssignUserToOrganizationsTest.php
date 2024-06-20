f7ae1c1401283c4be45bcd533706ed547f4cec0f
EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
2024-06-20 12:55:48 +0200

    - test CI

--- tests/Feature/AssignUserToOrganizationsTest.php
+++ tests/Feature/AssignUserToOrganizationsTest.php
@@ -54,7 +54,7 @@ class AssignUserToOrganizationsTest extends TestCase
         ]);
     }
 
-    public function testAssignFunctionWithNullResponse(): void
+    public function testAa(): void
     {
         $user = User::factory()->create();
 
