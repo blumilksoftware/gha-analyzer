6b485fe4ade9b5d918294f9ccf7370f2a66d6fbd
Wiktor-Wojciechowski <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-06-24 11:44:45 +0200

    cleanup

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,14 +1,7 @@
-<script >
+<script setup>
 import { Head } from '@inertiajs/vue3'
-import { hello } from './Hello.vue';
 
-export default {
-  data() {
-    return {
-      hello
-    };
-  }
-};
+var logs = []
 
 </script>
 <style>
@@ -21,7 +14,6 @@ export default {
     <Head>
         <title>GitHub Action simple analyzer</title>
     </Head>
-    <h1>{{hello}}</h1>
     <div class="container my-4 mx-auto bg-white rounded-xl p-4 shadow flex">
         <div class="flex-1 p-4">
             <textarea class="w-full text-xs font-mono shadow border rounded-xl p-3" :value="logs" rows="20"></textarea>
