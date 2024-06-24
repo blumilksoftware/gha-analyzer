eac05956b5174d55c88bc3b48b631a0e86da2d24
Wiktor-Wojciechowski <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-06-24 14:05:25 +0200

    laout fix in home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -16,9 +16,5 @@ const {logsSample} = useLogsStore()
     <Head>
         <title>GitHub Action simple analyzer</title>
     </Head>
-    <div class="container my-4 mx-auto bg-white rounded-xl p-4 shadow flex">
-        <div class="flex-1 p-4">
-            <textarea class="w-full text-xs font-mono shadow border rounded-xl p-3" :value="logsSample" rows="20"></textarea>
-        </div>
-    </div>
+    <textarea class="w-full text-xs font-mono shadow border rounded-xl p-3" :value="logsSample" rows="20"></textarea>
 </template>
