8741761444e3a515ff0eb301203f1aebae5a5db8
Wiktor-Wojciechowski <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-06-27 09:15:58 +0200

    In response to PR comments, moved sample data from DefaultLayout to a seperate json file

--- resources/js/Pages/DefaultLayout.vue
+++ resources/js/Pages/DefaultLayout.vue
@@ -3,29 +3,30 @@ import { useLogsStore } from '@/Stores/logsStore'
 import Navbar from './Navbar.vue'
 import { ref } from 'vue'
 
-const sample = `Date,Product,SKU,Quantity,Unit Type,Price Per Unit ($),Multiplier,Owner,Repository Slug,Username,Actions Workflow,Notes
-2022-05-20,Actions,Compute - UBUNTU,2,minute,0.008,1.0,galaxy,trantor,octocat,.github/workflows/check-pr.yml,
-2022-05-20,Actions,Compute - UBUNTU,9,minute,0.008,1.0,galaxy,caprica,octocat,.github/workflows/check-pr.yml,
-2022-05-20,Actions,Compute - UBUNTU,8,minute,0.008,1.0,galaxy,mustafar,octocat,.github/workflows/test.yml,
-2022-05-20,Actions,Compute - UBUNTU,9,minute,0.008,1.0,galaxy,caprica,octocat,.github/workflows/lint.yml,
-2022-05-20,Actions,Compute - UBUNTU,3,minute,0.008,1.0,galaxy,trantor,octocat,.github/workflows/behat.yml,
-2022-05-20,Actions,Compute - UBUNTU,3,minute,0.008,1.0,galaxy,mustafar,octocat,.github/workflows/check-pr.yml,
-2022-05-20,Actions,Compute - UBUNTU,2,minute,0.008,1.0,galaxy,trantor,octocat,.github/workflows/behat.yml,
-2022-05-20,Actions,Compute - UBUNTU,15,minute,0.008,1.0,galaxy,caprica,octocat,.github/workflows/lint.yml,
-2022-05-20,Actions,Compute - WINDOWS,7,minute,0.016,1.0,galaxy,mustafar,octocat,.github/workflows/test-windows.yml,
-2022-05-20,Actions,Compute - UBUNTU,11,minute,0.008,1.0,galaxy,caprica,octocat,.github/workflows/check-pr.yml,
-2022-05-20,Actions,Compute - UBUNTU,2,minute,0.008,1.0,galaxy,mustafar,octocat,.github/workflows/test.yml,
-2022-05-20,Actions,Compute - UBUNTU,1,minute,0.008,1.0,galaxy,trantor,dependabot[bot],.github/workflows/check-pr.yml,
-2022-05-20,Actions,Compute - UBUNTU,7,minute,0.008,1.0,galaxy,trantor,octocat,.github/workflows/behat.yml,
-`
-
 const {clearLogs, setLogs} = useLogsStore()
 
 const files = ref([])
 const fileInput = ref(null)
 
-function seed () {
+const sampleDataURL = '../../data/sampleData.json'
+
+async function fetchData(data) {
+  try {
+    const response = await fetch(new URL(data, import.meta.url).href);
+    const jsonData = await response.json();
+    return jsonData
+    
+  } catch (error) {
+    console.error('Error loading data:', error);
+  }
+}
+
+async function seed () {
+  const jsonData = await fetchData(sampleDataURL)
+  const sample = jsonData.sample
+  
   setLogs(sample)
+  
 }
 function clear () {
   clearLogs()
