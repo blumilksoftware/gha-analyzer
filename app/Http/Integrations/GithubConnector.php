feca7ce114385ecacd5c687e31a8e5b04de04856
orlowski11 <pawel.orlowski@studenci.collegiumwitelona.pl>
2024-07-01 13:32:43 +0200

    added rate limit handling

---
+++ app/Http/Integrations/GithubConnector.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Integrations;
+
+use Illuminate\Support\Facades\Cache;
+use Saloon\Http\Connector;
+use Saloon\Http\Response;
+use Saloon\RateLimitPlugin\Contracts\RateLimitStore;
+use Saloon\RateLimitPlugin\Helpers\RetryAfterHelper;
+use Saloon\RateLimitPlugin\Limit;
+use Saloon\RateLimitPlugin\Stores\LaravelCacheStore;
+use Saloon\RateLimitPlugin\Traits\HasRateLimits;
+use Saloon\Traits\Plugins\AlwaysThrowOnErrors;
+
+class GithubConnector extends Connector
+{
+    use AlwaysThrowOnErrors;
+    use HasRateLimits;
+
+    public function resolveBaseUrl(): string
+    {
+        return "https://api.github.com";
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            "Content-Type" => "application/json",
+            "Accept" => "application/json",
+        ];
+    }
+
+    protected function resolveLimits(): array
+    {
+        return [
+            Limit::allow(5000)->everyHour(),
+        ];
+    }
+
+    protected function resolveRateLimitStore(): RateLimitStore
+    {
+        return new LaravelCacheStore(Cache::store("redis"));
+    }
+
+    protected function handleTooManyAttempts(Response $response, Limit $limit): void
+    {
+        if ($response->status() !== 429 && $response->status() !== 403) {
+            return;
+        }
+
+        $limit->exceeded(
+            releaseInSeconds: RetryAfterHelper::parse($response->header("Retry-After")),
+        );
+    }
+}
---
+++ tests/Feature/RateLimiterTest.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Http\Integrations\GithubConnector;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
+use Saloon\Config;
+use Saloon\Enums\Method;
+use Saloon\Http\Faking\MockClient;
+use Saloon\Http\Faking\MockResponse;
+use Saloon\Http\Request;
+use Saloon\RateLimitPlugin\Exceptions\RateLimitReachedException;
+use Tests\TestCase;
+
+class RateLimiterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected GithubConnector $githubConnector;
+    protected string $requestUrl;
+    protected Request $request;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Config::preventStrayRequests();
+        Cache::clear();
+
+        $this->requestUrl = "https://api.github.com/test";
+        $this->githubConnector = new GithubConnector();
+
+        $this->request = new class() extends Request {
+            protected Method $method = Method::GET;
+
+            public function resolveEndpoint(): string
+            {
+                return "/test";
+            }
+        };
+
+        MockClient::destroyGlobal();
+    }
+
+    public function testRateLimitNotReached(): void
+    {
+        $mockClient = new MockClient([
+            $this->requestUrl => MockResponse::make(body: "", status: 200),
+        ]);
+
+        $this->githubConnector->withMockClient($mockClient);
+
+        $this->expectNotToPerformAssertions();
+
+        $this->githubConnector->send($this->request);
+    }
+
+    public function testRateLimitReached(): void
+    {
+        $mockClient = new MockClient([
+            $this->requestUrl => MockResponse::make(body: "", status: 200),
+        ]);
+
+        $this->githubConnector->withMockClient($mockClient);
+
+        $this->expectException(RateLimitReachedException::class);
+
+        for ($i = 0; $i = 5000; $i++) {
+            $this->githubConnector->send($this->request);
+        }
+    }
+
+    public function testResponse403(): void
+    {
+        $mockClient = new MockClient([
+            $this->requestUrl => MockResponse::make(body: "", status: 403),
+        ]);
+
+        $this->githubConnector->withMockClient($mockClient);
+
+        $this->expectException(RateLimitReachedException::class);
+
+        $this->githubConnector->send($this->request);
+    }
+
+    public function testResponse429(): void
+    {
+        $mockClient = new MockClient([
+            $this->requestUrl => MockResponse::make(body: "", status: 429),
+        ]);
+
+        $this->githubConnector->withMockClient($mockClient);
+
+        $this->expectException(RateLimitReachedException::class);
+
+        $this->githubConnector->send($this->request);
+
+        Cache::clear();
+    }
+}
