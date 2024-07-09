7647fe596596802b9be3227c7e66416644bcaf30
orlowski11 <pawel.orlowski@studenci.collegiumwitelona.pl>
2024-07-09 11:53:21 +0200

    cleanup

--- app/Jobs/FetchDataFromApi.php
+++ app/Jobs/FetchDataFromApi.php
@@ -15,7 +15,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 use Saloon\RateLimitPlugin\Helpers\ApiRateLimited;
 
 class FetchDataFromApi implements ShouldQueue
