991fdfbc93901c4f5f2473f7256df4222e7f066b
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-08-05 12:37:43 +0200

    add login page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.ts')
     @inertiaHead
 </head>
-<body>
-@inertia
+<body class="h-full bg-gradient-to-r from-white to-blue-400">
+    <div class="h-full container py-4 mx-auto">
+        @inertia
+    </div>
 </body>
 </html>
