91e77a8a51eb497938940908ca57c1460fda18f5
AmonDeShir <amonsir29@gmail.com>
2024-08-05 11:09:58 +0200

    remove pinia

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,10 +3,8 @@ import { createApp, h, type DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import DefaultLayout from './Layouts/DefaultLayout.vue'
-import { createPinia } from 'pinia'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
-const pinia = createPinia()
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
@@ -20,7 +18,6 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
-      .use(pinia)
       .mount(el)
   },
   progress: {
