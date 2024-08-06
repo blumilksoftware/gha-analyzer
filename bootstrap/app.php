3dde4f4cfba3177074715a507d7f9ae2eb562042
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-08-06 12:56:07 +0200

    add symfony response status codes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
 
+const HTTP_SESSION_EXPIRED = 419;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . "/../routes/web.php",
@@ -19,12 +21,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (!app()->environment(["local", "testing"]) && in_array($response->getStatusCode(), [500, 503, 404, 403], true)) {
+        $exceptions->respond(function (Response $response, Throwable $exception, Request $request): Response {
+            if (!app()->environment(["local", "testing"]) && in_array($response->getStatusCode(), [Response::HTTP_FORBIDDEN, Response::HTTP_INTERNAL_SERVER_ERROR, Response::HTTP_SERVICE_UNAVAILABLE, Response::HTTP_NOT_FOUND], true)) {
                 return Inertia::render("Errors/Error", ["status" => $response->getStatusCode()])
                     ->toResponse($request)
                     ->setStatusCode($response->getStatusCode());
-            } elseif ($response->getStatusCode() === 419) {
+            } elseif ($response->getStatusCode() === HTTP_SESSION_EXPIRED) {
                 return back()->with([
                     "message" => "The page expired, please try again.",
                 ]);
