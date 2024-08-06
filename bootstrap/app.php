ea4843c0ccfd2ec65abe7901180c80b1d6c8c731
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2024-08-06 12:20:05 +0200

    add custom rendering for exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,4 +19,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+            if (!app()->environment(["local", "testing"]) && in_array($response->getStatusCode(), [500, 503, 404, 403], true)) {
+                return Inertia::render("Errors/Error", ["status" => $response->getStatusCode()])
+                    ->toResponse($request)
+                    ->setStatusCode($response->getStatusCode());
+            } elseif ($response->getStatusCode() === 419) {
+                return back()->with([
+                    "message" => "The page expired, please try again.",
+                ]);
+            }
+
+            return $response;
+        });
     })->create();
